60ebbac482f4ea4771fbbb646470848289aab33b
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2019-12-10 10:40:54 +0100

    boucle for et conditon avec modulo

---
+++ asset/js/script.js
@@ -0,0 +1,13 @@
+let message = '<ul>';
+
+for (let i = -1722; i <= 2756 ; i++) {
+    if (i % 9 === 0) {
+        message += '<li>';
+        message += i;
+        message += '</li>';
+    }
+}
+
+message += '</ul>';
+alert('Aye !');
+document.write(message);
